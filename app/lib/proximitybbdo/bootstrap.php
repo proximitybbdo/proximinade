e9df28d1d7bf5ca1ac979c7ab827b29638bbbafc
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-04-10 22:06:05 +0200

    fix

--- app/lib/proximitybbdo/bootstrap.php
+++ app/lib/proximitybbdo/bootstrap.php
@@ -7,7 +7,6 @@ $lib_directory = $app_directory . 'lib/';
 $config_directory = $root_directory . 'config/';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . $lib_directory);
-set_include_path(get_include_path() . PATH_SEPARATOR . $app);
 set_include_path(get_include_path() . PATH_SEPARATOR . $config_directory);
 
 // [Limonade PHP](https://github.com/sofadesign/limonade/): the basis of this framework.
@@ -15,11 +14,6 @@ require_once('limonade.php');
 
 // Default timezone.
 date_default_timezone_set('Europe/Brussels');
-
-// Error reporting: report nothing, only fatal errors.
-error_reporting(E_ALL & ~E_STRICT & ~E_WARNING & ~E_NOTICE & ~E_DEPRECATED);
-// Display the errors when they occur.
-ini_set('display_errors', 1);
 
 // Load all Proximity BBDO libraries.
 foreach (glob($lib_directory . 'proximitybbdo/*.php') as $filename)
@@ -37,6 +31,12 @@ require_once('Zend/Loader.php');
 require_once('bootstrap.php');
 require_once('helpers.php');
 require_once('routes.php');
+
+// Models if exists
+if(file_exists($app_directory . 'models/')) {
+  foreach (glob($app_directory . 'models/*.php') as $filename)
+    require_once($filename);
+}
 
 function get_env() {
   global $root_directory;
@@ -96,11 +96,17 @@ function configure() {
   option('views_dir', $app_directory . 'views');
   option('controllers_dir', $app_directory . 'controllers');
 
-  // set correct error reporting
-  set_error_reporting();
+  foreach (glob($app_directory . 'models/*.php') as $filename)
+    require_once($filename);
 
   // default layout for rendering
   layout('layout.html.php');
+
+  // set correct error reporting
+  set_error_reporting();
+
+  if(function_exists('config_post'))
+    config_post();
 }
 
 // Start session
